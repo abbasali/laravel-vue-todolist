78a939bd4a0c7a04521dc79a4e9f2dc211d721df
chiranjeeb <chiranjeeb@ranium.in>
2019-11-30 13:15:57 +0530

    Npm Change

--- public/js/app.js
+++ public/js/app.js
@@ -52927,8 +52927,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/abbas/Sites/laravel-vue-todo/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/abbas/Sites/laravel-vue-todo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\laravel-vue-todolist\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel-vue-todolist\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
